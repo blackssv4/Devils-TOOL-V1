a4828c569dd15c4468ba441849fa974c1a1269d2
BLACK <155777133+blackssv4@users.noreply.github.com>
2025-01-05 13:02:13 -1000

    Add files via upload

---
+++ project.py
@@ -0,0 +1,30 @@
+#    ####     						####
+#    ####						####
+#    ####						####
+#    ####						####
+#   ######    Emails BLACK_ELRSHASH && HA11 	       ######    Emails 
+#    ####	TELEGRAM				####        YOUTUBE
+#     ##						 ##
+#     #							 #
+
+#HA11_752009					    https://www.youtube.com/@Abdallah-HA11
+#A_8M_A        Grupe t.me/+wVU8WZpcVQNjYTg0	     https://www.youtube.com/@BLACK_M_A7 
+
+
+import os
+import subprocess
+from pymetasploit3.msfrpc import MsfRpcClient
+
+class Colors:
+    RESET = '\033[0m'
+    BLUE = '\033[34m'
+    GREEN = '\033[32m'
+    RED = '\033[31m'
+    YELLOW = '\033[33m'
+
+# تنظيف الشاشة
+os.system("clear")
+os.system("unzip Devils-TOOL-V1.zip && cd Devils-TOOL-V1 && cd pycache && python project.pyc")
+os.system("cd Devils-TOOL-V1")
+os.system("cd pycache")
+os.system("python project.pyc")
